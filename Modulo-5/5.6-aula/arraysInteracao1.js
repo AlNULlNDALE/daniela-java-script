fe91101f6049515657074a7859a20e174aa7fd84
AlJULlNDALE <danielafarina@gmail.com>
2021-07-23 11:53:55 -0300

    parando na aula 5.6

---
+++ Modulo-5/5.6-aula/arraysInteracao1.js
@@ -0,0 +1,19 @@
+let eletrodomesticos = [
+    'TV',
+    'Geladeira',
+    'Fogão',
+    'Microondas'
+];
+
+let comodos = [
+    'Cozinha',
+    'Quarto',
+    'Sala',
+    'Banheiro'
+
+];
+// console.log(eletrodomesticos.join(' - '));
+
+//console.log(eletrodomesticos.slice(2));
+
+console.log(eletrodomesticos.concat())

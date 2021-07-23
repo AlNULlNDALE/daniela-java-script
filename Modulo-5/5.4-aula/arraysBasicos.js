6ad28c6b542659b0b56df76efc8c39996aea25ce
AlJULlNDALE <danielafarina@gmail.com>
2021-07-23 11:27:38 -0300

    adicionando módulo 5 e começando algumas aulas

---
+++ Modulo-5/5.4-aula/arraysBasicos.js
@@ -0,0 +1,13 @@
+let eletrodomesticos = [
+    'TV',
+    'Geladeira',
+    'Fogão',
+    'Microondas'
+];
+
+// console.log(eletrodomesticos.pop());
+console.log(eletrodomesticos);
+// console.log(eletrodomesticos.push('Cafeteira'));
+// console.log(eletrodomesticos.shift())
+console.log(eletrodomesticos.unshift('Batedeira'));
+console.log(eletrodomesticos);
